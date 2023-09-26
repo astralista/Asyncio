e8f5554b9e5b67a4e4fc0655d29cb06af3ac7ba7
Godz Dmitriy <woke@mail.ru>
2023-09-26 18:33:22 +0300

    gotovo ;p

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import JSON, Column, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -25,4 +25,16 @@ Base = declarative_base()
 class SwapiPeople(Base):
     __tablename__ = "swapi_people"
     id = Column(Integer, primary_key=True)
-    json = Column(JSON)
+    birth_year = Column(String)
+    eye_color = Column(String)
+    films = Column(String)
+    gender = Column(String)
+    hair_color = Column(String)
+    height = Column(String)
+    homeworld = Column(String)
+    mass = Column(String)
+    name = Column(String)
+    skin_color = Column(String)
+    species = Column(String)
+    starships = Column(String)
+    vehicles = Column(String)
--- swapi_async.py
+++ swapi_async.py
@@ -9,17 +9,71 @@ from models import Base, Session, SwapiPeople, engine
 CHUNK_SIZE = 5
 
 
-async def get_people(client, people_id):
-    response = await client.get(f"https://swapi.py4e.com/api/people/{people_id}")
-    json_data = await response.json()
-    return json_data
+async def fetch(session, url):
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            json_data = await response.json()
+            return json_data
+    except aiohttp.ClientResponseError:
+        pass
+    except Exception:
+        pass
+    return None
 
 
 async def insert_to_db(results):
     async with Session() as session:
-        swapi_people_list = [SwapiPeople(json=item) for item in results]
-        session.add_all(swapi_people_list)
+        people_list = []
+        for person_json in results:
+            if person_json and person_json.get("url"):
+                films = await get_info(session, person_json.get("films"))
+                homeworld = await get_info(session, [person_json.get("homeworld")])
+                species = await get_info(session, person_json.get("species"))
+                starships = await get_info(session, person_json.get("starships"))
+                vehicles = await get_info(session, person_json.get("vehicles"))
+
+                if None not in (films, homeworld, species, starships, vehicles):
+                    films_titles = [film["title"] for film in films if film is not None]
+                    species_names = [
+                        specie["name"] for specie in species if specie is not None
+                    ]
+                    starships_names = [
+                        starship["name"]
+                        for starship in starships
+                        if starship is not None
+                    ]
+                    vehicles_names = [
+                        vehicle["name"] for vehicle in vehicles if vehicle is not None
+                    ]
+
+                    person = SwapiPeople(
+                        id=int(person_json["url"].split("/")[-2]),
+                        birth_year=person_json["birth_year"],
+                        eye_color=person_json["eye_color"],
+                        films=", ".join(films_titles),
+                        gender=person_json["gender"],
+                        hair_color=person_json["hair_color"],
+                        height=person_json["height"],
+                        homeworld=homeworld[0]["name"]
+                        if homeworld[0] is not None
+                        else None,
+                        mass=person_json["mass"],
+                        name=person_json["name"],
+                        skin_color=person_json["skin_color"],
+                        species=", ".join(species_names),
+                        starships=", ".join(starships_names),
+                        vehicles=", ".join(vehicles_names),
+                    )
+                    people_list.append(person)
+
+        session.add_all(people_list)
         await session.commit()
+
+
+async def get_info(session, urls):
+    tasks = [fetch(session, url) for url in urls]
+    return await asyncio.gather(*tasks)
 
 
 async def main():
@@ -27,16 +81,18 @@ async def main():
         await con.run_sync(Base.metadata.drop_all)
     async with engine.begin() as con:
         await con.run_sync(Base.metadata.create_all)
+
     async with aiohttp.ClientSession() as client:
-        for ids_chunk in chunked(range(1, 100), CHUNK_SIZE):
-            coros = [get_people(client, i) for i in ids_chunk]
+        for ids_chunk in chunked(range(100), CHUNK_SIZE):
+            coros = [
+                fetch(client, f"https://swapi.py4e.com/api/people/{i}/")
+                for i in ids_chunk
+            ]
             results = await asyncio.gather(*coros)
-            insert_to_db_coro = insert_to_db(results)
-            asyncio.create_task(insert_to_db_coro)
+            await insert_to_db(results)
+
     current_task = asyncio.current_task()
-    tasks_to_await = asyncio.all_tasks() - {
-        current_task,
-    }
+    tasks_to_await = asyncio.all_tasks() - {current_task}
     for task in tasks_to_await:
         await task
 
